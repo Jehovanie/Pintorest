3f15e0b621e872fcc7fd86c8fbca60e58e5b8a13
Jehovanie <jehovanieram@gmail.com>
2022-02-26 21:15:06 +0300

    Refactoring show/home/create/update

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,19 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Form\PinType;
 use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home" , methods="GET")
      */
     public function index(PinRepository $repo): Response
     {
@@ -24,7 +23,7 @@ class PinsController extends AbstractController
     }
 
     /**
-     * @Route("/pins/{id<[0-9]+>}", name="app_show_pin")
+     * @Route("/pins/{id<[0-9]+>}", name="app_show_pin" , methods="GET")
      */
     public function show(PinRepository $repo, int $id): Response
     {
@@ -36,22 +35,22 @@ class PinsController extends AbstractController
     /**
      * @Route("/pins/create", name="app_create_pin" , methods="GET|POST")
      */
-    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    public function create(Request $request, EntityManagerInterface $entityManager, Pin $pin = null): Response
     {
-        $form = $this->createFormBuilder()
-            ->add("title", TextType::class)
-            ->add("description", TextareaType::class)
-            ->getform();
+        if ($pin === null) {
+            $pin = new Pin;
+        }
+
+        $form = $this->createForm(PinType::class, $pin);
+        // $form = $this->createFormBuilder($pin)
+        //     ->add("title", TextType::class)
+        //     ->add("description", TextareaType::class)
+        //     ->getform();
+
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-
-            $pin = new Pin;
-
-            $pin->setTitle($data['title']);
-            $pin->setDescription($data['description']);
 
             $entityManager->persist($pin);
             $entityManager->flush();
@@ -60,7 +59,8 @@ class PinsController extends AbstractController
         }
 
         return $this->render('pins/new.html.twig', [
-            "formulaire" => $form->createView()
+            "formulaire" => $form->createView(),
+            "pin" => $pin
         ]);
     }
 
@@ -71,36 +71,13 @@ class PinsController extends AbstractController
     {
 
         $pin = $repo->find($id);
+        $result = $this->create($request, $entityManager, $pin);
 
-        $form = $this->createFormBuilder($pin)
-            ->add("title", TextType::class, ['attr' => ["value" => $pin->getTitle()]])
-            ->add("description", TextareaType::class)
-            ->getform();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $data = $form->getData();
-
-            $pin->setTitle($data->getTitle());
-            $pin->setDescription($data->getDescription());
-
-            $pin->updateTimestamp();
-
-            $entityManager->persist($pin);
-            $entityManager->flush();
-
-            return $this->redirectToRoute("app_show_pin", ["id" => $pin->getId()]);
-        }
-
-        return $this->render('pins/update.html.twig', [
-            "formulaire" => $form->createView()
-        ]);
+        return $result;
     }
 
     /**
-     * @Route("/pins/delete/{id<[0-9]+>}", name="app_delete_pin" , methods="GET|POST")
+     * @Route("/pins/delete/{id<[0-9]+>}", name="app_delete_pin" , methods="GET|POST|DELETE")
      */
     public function delete(PinRepository $repo, EntityManagerInterface $entityManager, int $id): Response
     {
@@ -108,9 +85,6 @@ class PinsController extends AbstractController
         $pin = $repo->find($id);
         $entityManager->remove($pin);
         $entityManager->flush();
-
-        // $pins = $repo->findBy([], ['createdAt' => 'DESC']);
-        // return $this->render('pins/home.html.twig', ["pins" => $pins]);
 
         return $this->index($repo);
     }
