e6ad4df3e26eb86a11c2f1b17488872ced4bb7a8
Jehovanie <jehovanieram@gmail>
2022-06-11 09:58:10 +0200

    donner les directives pour lancer l'application dans README.md

--- README.md
+++ README.md
@@ -22,3 +22,16 @@ Manipulation des images pour les projects dévélopper en symfony .
 
 
 Possible que la taille de l'image est trés gros taille donc on utilise imagick
+
+========================================================================================================
+
+ETAPE DE TEST :
+    1 - composer install
+    2 - npm install ( important :  node version (14.19.0) , npm version( 6.14.16 )) , npm run dev
+    3 - edit l'environement .env
+    4 - php bin/console doctrine:database:create
+    5 - php bin/console make:migration
+    6 - php bin/console doctrine:migrations:migrate
+
+    7 - allez creer un utilisateur.
+    8 - aller creer des pins.
--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class PinsController extends AbstractController
 {
     /**
@@ -19,7 +20,8 @@ class PinsController extends AbstractController
      */
     public function index(PinRepository $repo): Response
     {
-        $pins = $repo->findBy([], ['createdAt' => 'DESC']);
+        // $pins = $repo->findBy([], ['createdAt' => 'DESC']);
+        $pins = [];
         return $this->render('pins/home.html.twig', ["pins" => $pins]);
     }
 
@@ -40,7 +42,7 @@ class PinsController extends AbstractController
         //     ->add("description", TextareaType::class)
         //     ->getform();
 
-
+        /* LORSQUE ON DEMANDE AVEC LE METHODE POST (envoie des informations via le formulaire) */
         $form->handleRequest($request);
 
         ///after the submitted form
@@ -59,6 +61,7 @@ class PinsController extends AbstractController
             return $this->redirectToRoute("app_show_pin", ["id" => $pin->getId()]);
         }
 
+        /* DEMANDE SYMPLEMENT DE LA PAGE QUI CONTIENT LA FORMULAIRE A REMPLIR */
         return $this->render('pins/new.html.twig', [
             "formulaire" => $form->createView(),
             "pin" => $pin
@@ -74,30 +77,35 @@ class PinsController extends AbstractController
         if ($pin) {
             return $this->render('pins/show.html.twig', compact("pin"));
         }
+        return $this->redirectToRoute("app_home");
     }
 
 
-    /**
+    /** 
      * @Route("/pins/{id<[0-9]+>}/update", name="app_update_pin" , methods="GET|POST")
      */
     public function update(Request $request, Pin $pin, EntityManagerInterface $entityManager): Response
     {
 
         ///create form and set their default value to $pin and return $pin with new value of pin.
+        ///herite vient de la class AbstracController
         $form = $this->createForm(PinType::class, $pin);
 
+        ///envoie des informations via le requette
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
 
             $entityManager->persist($pin);
             $entityManager->flush();
 
+            ///add message flash
             $this->addFlash("info", "Pin succesfull updated...");
 
+            ///redirection
             return $this->redirectToRoute("app_show_pin", ["id" => $pin->getId()]);
         }
 
+        ///rendre de la page formulaire pour l'action de modifier l'pin
         return $this->render('pins/update.html.twig', [
             "formulaire" => $form->createView(),
             "pin" => $pin
@@ -109,10 +117,11 @@ class PinsController extends AbstractController
      */
     public function delete(Pin $pin, EntityManagerInterface $entityManager): Response
     {
+        ///provoque une message flash heriter vient la class AbstracController
         $this->addFlash("error", "Pin succesfull deleted...");
 
+        ////entityManager responsable de l'action avec l'ORM
         $entityManager->remove($pin);
-
         $entityManager->flush();
 
         return $this->redirectToRoute("app_home");
